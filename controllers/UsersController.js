3fab476031f32407e784e83f2c56997bdb3ab720
Alagbada123 <114651605+Alagbada123@users.noreply.github.com>
2025-06-23 21:49:35 +0200

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,31 +1,44 @@
 #!/usr/bin/node
 
-const dbClient = require('../utils/db');
+import sha1 from 'sha1';
+import Bull from 'bull';
+import { ObjectId } from 'mongodb';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const userQueue = new Bull('userQueue');
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-      res.end();
-      return;
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+
+    const usersCollection = await dbClient.users();
+    const user = await usersCollection.findOne({ email });
+    if (user) return res.status(400).send({ error: 'Already exist' });
+
+    const result = await usersCollection.insertOne({ email, password: sha1(password) });
+    const userId = result.insertedId.toString();
+
+    userQueue.add({ userId }); // Add job to the queue for welcome email
+
+    return res.status(201).send({ id: userId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (userId) {
+      const user = await (await dbClient.users()).findOne({ _id: new ObjectId(userId) });
+      if (user) {
+        return res.status(200).send({ id: user._id, email: user.email });
+      }
     }
-    if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-      res.end();
-      return;
-    }
-    const userExist = await dbClient.userExist(email);
-    if (userExist) {
-      res.status(400).json({ error: 'Already exist' });
-      res.end();
-      return;
-    }
-    const user = await dbClient.createUser(email, password);
-    const id = `${user.insertedId}`;
-    res.status(201).json({ id, email });
-    res.end();
+    return res.status(401).send({ error: 'Unauthorized' });
   }
 }
 
-module.exports = UsersController;
+export default UsersController;
