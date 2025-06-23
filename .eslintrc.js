8ca4db887d1f657b94c785397649805333b0add6
Alagbada123 <114651605+Alagbada123@users.noreply.github.com>
2025-06-23 22:49:44 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,47 @@
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
-    },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+    mocha: true, // Informs ESLint about Mocha's global functions like 'describe' and 'before'
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    // --- General Rules ---
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'max-classes-per-file': 'off',
-      'no-underscore-dangle': 'off',
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
-    },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
+
+    // --- Jest Plugin Rule Overrides ---
+    // These are turned OFF because they conflict with the project's use of Mocha/Chai
+
+    'jest/valid-expect': 'off', // Turns off errors for Chai's `expect(...).to.equal(...)`
+    'jest/prefer-expect-assertions': 'off', // No longer requires `expect.hasAssertions()`
+    'jest/lowercase-name': 'off', // Allows `describe('Utils', ...)` with uppercase
+    'jest/prefer-strict-equal': 'off', // Allows the use of `.toEqual()` or Chai's `.equal()`
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
