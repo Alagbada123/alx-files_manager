69ef261b4706a9d84f925533b2828897aa21fa90
Alagbada123 <114651605+Alagbada123@users.noreply.github.com>
2025-06-23 22:49:01 +0200

    Update utils.test.js

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,36 +1,35 @@
 import { expect } from 'chai';
-import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-describe('Utils', () => {
+describe('Utils Test Suite', () => {
   describe('RedisClient', () => {
     it('isAlive returns true when connected', () => {
-      expect.hasAssertions();
-      expect(redisClient.isAlive()).toEqual(true);
+      expect(redisClient.isAlive()).to.equal(true);
     });
   });
 
   describe('DBClient', function () {
+    // Increase timeout to allow for db connection
     this.timeout(10000);
+
+    // This hook runs before the tests in this block, waiting for the connection
     before((done) => {
-      setTimeout(done, 3000); // Wait for db to connect
+      setTimeout(done, 3000);
     });
 
     it('isAlive returns true when connected', () => {
-      expect.hasAssertions();
-      expect(dbClient.isAlive()).toEqual(true);
+      expect(dbClient.isAlive()).to.equal(true);
     });
 
     it('nbUsers returns a number', async () => {
-      expect.hasAssertions();
       const numUsers = await dbClient.nbUsers();
-      expect(typeof numUsers).toEqual('number');
+      expect(numUsers).to.be.a('number');
     });
 
     it('nbFiles returns a number', async () => {
-      expect.hasAssertions();
       const numFiles = await dbClient.nbFiles();
-      expect(typeof numFiles).toEqual('number');
+      expect(numFiles).to.be.a('number');
     });
   });
 });
