a8c8f1e0159e176d02d3c4c2e7b64b2ba6efcac1
Alagbada123 <114651605+Alagbada123@users.noreply.github.com>
2025-06-23 22:32:41 +0200

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,45 +1,43 @@
 #!/usr/bin/node
 
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
-const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || "files_manager";
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
   constructor() {
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.connected = false;
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
     this.client.connect((err) => {
       if (!err) {
-        this.connected = true;
         this.db = this.client.db(DB_DATABASE);
       } else {
-        console.log("MongoDB connection error:", err);
-        this.connected = false;
+        console.log('MongoDB connection error:', err);
+        this.db = false;
       }
     });
   }
 
   isAlive() {
-    return this.connected;
+    return !!this.db;
   }
 
   async nbUsers() {
-    return this.db.collection("users").countDocuments();
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.db.collection("files").countDocuments();
+    return this.db.collection('files').countDocuments();
   }
 
   async users() {
-    return this.db.collection("users");
+    return this.db.collection('users');
   }
 
   async files() {
-    return this.db.collection("files");
+    return this.db.collection('files');
   }
 }
 
